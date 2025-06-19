1e875a132a1835c5781f449cfd39705dc1feb0d8
Rice Dilla <rice.dillla@gmail.com>
2025-06-19 14:23:43 +0300

    Fix greeting message in server response from 'Hello, world!' to 'Hello, www!'

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const PORT = 3000;
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello, world!\n');
+  res.end('Hello, www!\n');
 }).listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
